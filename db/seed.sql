01245dc04f8aee4c3ced14ab60f086b398527219
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-05-12 17:53:00 -0400

    fixed code in deeds.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,20 +1,29 @@
 INSERT INTO department (department_name)
 VALUES
-    ("HR"),
+    ("IT"),
     ("Sales"),
-    ("Production"),
-    ("Fianace");
+    ("Operations"),
+    ("Finance");
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-("Mangaer", 100000, 1),
-("Sales associate", 80000, 2),
-("cashier", 30000, 3);
+('Full Stack Developer', 200000, 1),
+('Software Engineer', 150000, 1),
+('Marketing Coordindator', 85000, 2), 
+('Sales Lead', 90000, 2),
+('Project Manager', 100000, 3),
+('Operations Manager', 110000, 3),
+('Accountant', 45000, 4), 
+('Finanical Analyst', 150000, 4);
 
 INSERT INTO employee ( first_name, last_name, role_id, manager_id)
 VALUES
 ("Ebony", "Peralta", 1, NULL),
 ("Samira", "Davies", 2, 1),
 ("Paisley", "stone", 3, 2),
+("Lacey", "Mendoza", 4, 3),
+("Lacey", "Mendoza", 4, 3),
+("Lacey", "Mendoza", 4, 3),
+("Lacey", "Mendoza", 4, 3),
 ("Lacey", "Mendoza", 4, 3);
 
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ prompt([
         type: 'list',
         name: 'options',
         message: 'What would you like to do?',
-        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
     }
 ])
     .then(option => {
